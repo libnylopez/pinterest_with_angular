60b666747720a376e2956f26dc13f063dd9dba4a
libnylopez <saquiclopez1@gmail.com>
2025-11-19 22:47:46 -0600

    Add environments folder with Firebase config

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  databaseURL: 'https://pinterest-1ba3f-default-rtdb.firebaseio.com'
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  databaseURL: 'https://pinterest-1ba3f-default-rtdb.firebaseio.com'
+};
